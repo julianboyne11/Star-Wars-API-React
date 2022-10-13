7d24f10ffb7ed3f850550ae83632c0f49c12b18a
julianboyne <julianboyne11@gmail.com>
2022-10-13 19:37:51 -0400

    add css to nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import "./NavBar.css"
 
 const NavBar = () => {
   return (
     <header className="App-header">
-      <Link to="/starship-list">Startship List</Link>
+      <Link id="link" to="/starship-list"> Start Wars Startships</Link>
     </header>
   );
 }
--- src/pages/Starship List/Starship List.jsx
+++ src/pages/Starship List/Starship List.jsx
@@ -24,7 +24,7 @@ const StartshipList = () => {
       <>
       <div className="all-ships">
         {starships.map(ship =>
-        <Link key={ship.name} to="/starship" state={{ ship }}> 
+        <Link id="link" key={ship.name} to="/starship" state={{ ship }}> 
         <h2 className="ship-container">{ship.name}</h2>
         </Link>
         )}
