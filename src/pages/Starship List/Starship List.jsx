dbd79f7afbb0e0426e395887dfdc326e15af0c05
julianboyne <julianboyne11@gmail.com>
2022-10-13 17:08:07 -0400

    implement minor css to starship list

--- src/pages/Starship List/Starship List.jsx
+++ src/pages/Starship List/Starship List.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import { getStarshipList } from "../../services/sw-calls"
 import { Link } from "react-router-dom";
-
+import "./StarshipList.css"
 
 
 
@@ -20,8 +20,12 @@ const StartshipList = () => {
   return ( 
     <>
       <h1>Starships</h1>
-      <div>
-        {starships.map(ship =><Link key={ship.name} to="/starship" state={{ ship }}> {ship.name}</Link>)}
+      <div className="all-ships">
+        {starships.map(ship =>
+        <Link key={ship.name} to="/starship" state={{ ship }}> 
+        <h2 className="ship-container">{ship.name}</h2>
+        </Link>
+        )}
       </div>
     </>
   );
