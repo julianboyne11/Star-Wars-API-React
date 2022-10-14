0b5ef24bdf42446961e4507a8e8f845384f0c3d2
julianboyne <julianboyne11@gmail.com>
2022-10-13 20:59:32 -0400

    change css color background and font

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import { Route, Routes } from "react-router-dom"
 import NavBar from './components/NavBar/NavBar'
 import StartshipList from './pages/Starship List/Starship List';
-import Ship from './pages/Ship/Ship';
+import Ship from './pages/Ship/Ship'
 
 function App() {
   return (
--- src/pages/Starship List/Starship List.jsx
+++ src/pages/Starship List/Starship List.jsx
@@ -20,16 +20,12 @@ const StartshipList = () => {
   return ( 
     <>
       <h1>Starships</h1>
-      <div>
-      <>
-      <div className="all-ships">
-        {starships.map(ship =>
-        <Link id="link" key={ship.name} to="/starship" state={{ ship }}> 
-        <h2 className="ship-container">{ship.name}</h2>
-        </Link>
-        )}
-      </div>
-      </>
+        <div className="all-ships">
+          {starships.map(ship =>
+            <Link id="link" key={ship.name} to="/starship" state={{ ship }}> 
+            <h2 className="ship-container">{ship.name}</h2>
+            </Link>
+          )}
       </div>
     </>
   );
